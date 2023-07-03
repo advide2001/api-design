f9c1bbd9f9b19739310049b93ad41fdb70d1ec2b
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-03 18:53:27 +0530

    feat: Add route handlers for user creation and signin

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router";
 import { protect } from "./modules/auth";
 import morgan from "morgan";
+import { createNewUser, signin } from "./handlers/user";
 
 const app = express();
 
@@ -25,5 +26,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signin);
 
 export default app;
