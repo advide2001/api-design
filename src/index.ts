d5cce2f3f4d8ecdeab3d6785951fd3a81529f0f5
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-03 16:38:55 +0530

    feat: Configure environment variables using dotenv

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@
 
 // WITH EXPRESS
 import app from "./server";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 app.listen(3001, () => {
   console.log("hello on port http://localhost:3001");
