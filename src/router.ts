2bfad327e21eacf69418107541190fc5c8a23e9b
Adarsh Desai <desaiadarsh22@gmail.com>
2023-07-04 11:36:32 +0530

    refactor: Restructure router and route handlers

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,70 @@
 import { Router } from "express";
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
+import {
+  createProduct,
+  deleteProduct,
+  getOneProduct,
+  getProducts,
+  updateProduct,
+} from "./handlers/product";
 
 const router = Router();
 
 // product Routes
-router.get("/product", (req, res) => {
-  res.json({ message: req.body.secret });
-});
-router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
-router.post("/product", () => {});
-router.delete("/product/:id", () => {});
+router.get("/product", getProducts);
+router.get("/product/:id", getOneProduct);
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  updateProduct
+);
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputErrors,
+  createProduct
+);
+router.delete("/product/:id", deleteProduct);
 
 // update Routes
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update", () => {});
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  body("version").optional(),
+  handleInputErrors,
+  () => {}
+);
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  handleInputErrors,
+  () => {}
+);
 router.delete("/update/:id", () => {});
 
 // update point Routes
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("/updatepoint", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
+router.post(
+  "/updatepoint",
+  body("name").exists().isString(),
+  body("description").exists().isString(),
+  body("updateID").exists().isString(),
+  () => {}
+);
 router.delete("/updatepoint/:id", () => {});
 
 export default router;
